3f6819b59db0901079073dca0c210a2be5bb087d
Barazii <mahmoud.baraziii@gmail.com>
2025-04-19 21:54:05 +0200

    Learning cmake

--- test/test.cpp
+++ test/test.cpp
@@ -11,9 +11,4 @@ BOOST_AUTO_TEST_CASE(test_elapsed_time_zero)
     BOOST_CHECK_EQUAL(Format::ElapsedTime(0), "00:00:00");
 }
 
-BOOST_AUTO_TEST_CASE(test_linux_parser_uid)
-{
-    BOOST_CHECK_EQUAL(LinuxParser::User(3544), "0");
-}
-
 BOOST_AUTO_TEST_SUITE_END()
