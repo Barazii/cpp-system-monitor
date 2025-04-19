0a5edc3465d1cf388ce8f3019a3a502f67c2a7a8
Barazii <mahmoud.baraziii@gmail.com>
2025-04-19 13:04:31 +0200

    Removed build directory

--- build/CMakeFiles/monitor.dir/src/processor.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/monitor.dir/src/processor.cpp.o: \
- /home/mahmood/CppND-System-Monitor-Project-Updated/src/processor.cpp \
- /usr/include/stdc-predef.h \
- /home/mahmood/CppND-System-Monitor-Project-Updated/include/processor.h
